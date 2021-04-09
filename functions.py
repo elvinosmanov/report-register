3e87dda4a5259739658a38e71e701b40fd9e22e0
elvinosmanov <youngtronik@gmail.com>
2021-04-09 14:42:57 +0400

    find_path created

--- functions.py
+++ functions.py
@@ -14,4 +14,26 @@ def create_folder(path):
 # It will return dataframe with sepcific columns: 
 def custom_df(df, column_names):
     return df.loc[:,column_names]
+# Function will find the path according to filename:
+def find_path(filename):
+    company_id = filename[0:2]
+    first = filename[5:7]
+    second = filename[10:12]
+    third = filename[13:16]
+
+
+    if(company_id=='BP' or company_id == 'AC'):
+        return glob.glob("R:\\Mechanical_Completion\\VND\\*"+filename+'*.pdf')[0]
     
+    else:
+        if(company_id == 'AF'):
+            path = f'AZFEN\\Drawings\\HVAC\\{first}'
+        elif(company_id == 'EM'):
+            path = f'EMTUNGA\\Drawings\\{second}\\{third}'
+        elif(company_id == 'BR'):
+            path = f'KBR\\Drawings\\{second}\\{third}\\{first}'
+        elif(company_id=="BP"):
+            path = f'R:\\Mechanical_Completion\\VND'
+        elif(company_id=='OV'):
+            path = f'NOV\\Drawings\\{second}\\{third}\\{first}'
+        return glob.glob("R:\\DCC\\ACE DCC\\"+path+'\\*'+filename+'*.pdf')[0]
